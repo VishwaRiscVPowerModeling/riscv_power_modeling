43cf1eda1d6a88b57ec4b00317442864ee5abfff
VishwaRiscV <115461196+VishwaRiscVPowerModeling@users.noreply.github.com>
2023-04-20 06:49:33 +0530

    Update hazard_unit.sv

--- hazard_unit.sv
+++ hazard_unit.sv
@@ -4,6 +4,12 @@ input logic [INST_WIDTH -1:0] ifid_ir;
 input logic [REGISTER_FILE_ADDRESS_WIDTH -1:0] ifid_rs1,ifid_rs2,idex_rd,exmem_rd;
 output logic pc_hold, ifid_hold, ctrl_hold; 
 
+    
+    /*
+    Hazarding unit detect when there is incorrect data usage for calculation is expected happen.
+    This unit holds operations of the processor untill correct data reaches to the pipeline registers 
+    
+    */
 always_comb begin 
     if ( idex_ctrl_mem_r && ((idex_rd == ifid_rs1) | (idex_rd == ifid_rs2))) begin
     pc_hold = 1'b1;
