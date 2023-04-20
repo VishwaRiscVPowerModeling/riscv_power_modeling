e53045d8c223733085628570bd06457cfcac2603
VishwaRiscV <115461196+VishwaRiscVPowerModeling@users.noreply.github.com>
2023-04-20 06:19:32 +0530

    Update branch_comparator.sv

--- branch_comparator.sv
+++ branch_comparator.sv
@@ -19,15 +19,15 @@ output logic branch_eq_flag;
 wire [RISC_V_DATA_WIDTH - 1 : 0] xor_r_data;
 
 
-assign xor_r_data = r_data_0 ^ r_data_1;
-assign branch_eq_flag = ! ( | xor_r_data);
+assign xor_r_data = r_data_0 ^ r_data_1;                // compare two regesiter values at idex stage 
+    assign branch_eq_flag = ! ( | xor_r_data);          // if both register values are matching --> branch equal flag set to 1
 
 
 always@(posedge clk or posedge rst) begin
     if(rst) branch_decision_incorrect_flag = 1'b0;
     else begin 
     if (idex_ir[6:0] ==  7'b1100011) begin 
-        if (branch_eq_flag == ~idex_branch_decision )
+        if (branch_eq_flag == ~idex_branch_decision )   //compares the branch predictor desicision with actual branch decision at idex stage 
             branch_decision_incorrect_flag = 1'b1;
 
         else  branch_decision_incorrect_flag =1'b0;
